2b5c85a63a959a14d6d6b5b886fda4b3f2f9ed1e
CaitW <wolters.caitlin@gmail.com>
2015-01-09 14:29:33 -0600

    Adding layers via omnivore and php

--- js/main.js
+++ js/main.js
@@ -1,15 +1,56 @@
-var map;
+var map, basemaps, overlays;
 
 $(document).ready(function() {
 
 	map = L.map('map').setView([31.922, -106.044], 16);
 
-	var basemaps = 
+	basemaps = 
 	{
 		"imagery": L.tileLayer("http://otile1.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.jpg").addTo(map)		
 	};
 
-	
+	overlays = {
+		regions: {},
+		areas: {},
+		trails: {},
+		boulders: {},
+		routes: {}
+	}
+
+	$.getJSON("./php/getFiles.php", function (data) {
+
+		$.each(data.regions, function (index, value) { 
+			overlays.regions[index] = omnivore.kml("./data/Hueco/kml/regions/" + value).addTo(map);
+		});
+
+		$.each(data.areas, function (index, value) { 
+
+			overlays.areas[index] = omnivore.kml("./data/Hueco/kml/areas/" + value).addTo(map);
+
+		});
+
+		$.each(data.trails, function (index, value) { 
+
+				overlays.trails[index] = omnivore.kml("./data/Hueco/kml/trails/" + value).addTo(map);
+			
+		});
+
+		$.each(data.boulders, function (index, value) { 
+
+				overlays.boulders[index] = omnivore.kml("./data/Hueco/kml/boulders/" + value).addTo(map);
+			
+		});
+
+		$.each(data.routes, function (index, value) { 
+
+				overlays.routes[index] = omnivore.kml("./data/Hueco/kml/routes/" + value).addTo(map);
+			
+		});
+
+		
+ 
+
+	});
 
 	
 });
--- php/getFiles.php
+++ php/getFiles.php
@@ -1,8 +1,26 @@
 <?php
 
-	$files = scandir('/');
-	print_r($files);
+	chdir("..");
+	chdir("data/Hueco/kml");
+	$folders = scandir(getcwd());
+	$json_folders = array();
 
+	for ($x = 2; $x < count($folders); $x++)
+	{
+		$nameOfFolder = $folders[$x];
+		chdir($nameOfFolder);
+		$files = scanDir(getcwd());
+		$json_folders[$nameOfFolder] = array();
+
+		for ($y = 2; $y < count($files); $y++)
+		{
+			array_push($json_folders[$nameOfFolder], $files[$y]);
+		}
+		
+		chdir("..");
+	}
+
+	echo json_encode($json_folders, JSON_PRETTY_PRINT);
 
 
 ?>
