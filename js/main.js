a2f00445a49195d24cfceb9eb97824206690606c
CaitW <wolters.caitlin@gmail.com>
2015-01-09 13:40:09 -0600

    More Data

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+var map;
 
+$(document).ready(function() {
+
+	map = L.map('map').setView([31.922, -106.044], 16);
+
+	var basemaps = 
+	{
+		"imagery": L.tileLayer("http://otile1.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.jpg").addTo(map)		
+	};
+
+	
+
+	
+});
---
+++ php/getFiles.php
@@ -0,0 +1,8 @@
+<?php
+
+	$files = scandir('/');
+	print_r($files);
+
+
+
+?>
