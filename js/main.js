1f100c8526ac00ef28f9ad7586e2f71031ba82fb
CaitW <wolters.caitlin@gmail.com>
2015-01-22 09:14:07 -0700

    Added tiles for North, boulders, trails

--- js/main.js
+++ js/main.js
@@ -7,13 +7,17 @@ $(document).ready(function() {
 	basemaps = 
 	{
 		"lowResImagery": L.tileLayer("http://otile1.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.jpg", {
-			minZoom: 0,
-			maxZoom: 18,
-		}).addTo(map),	
-		"hiResImagery": L.tileLayer("http://a.basemaps.cartocdn.com/light_all/{z}/{y}/{x}.png", {
-			minZoom: 19,
-			maxZoom: 20
-		}).addTo(map)			
+			minZoom: 16,
+			maxZoom: 18
+		}).addTo(map),
+		"NorthMountain": L.tileLayer("./data/Hueco/tiles/north_mountain/{z}/{x}/{y}.png", {
+			minZoom: 16,
+			maxZoom: 18
+		}).addTo(map).setOpacity(0.7),
+		"areas": L.tileLayer("./data/Hueco/tiles/areas/{z}/{x}/{y}.png", {
+			minZoom: 16,
+			maxZoom: 18
+		}).addTo(map)
 	};
 
 	//set each to a layer group
@@ -26,14 +30,6 @@ $(document).ready(function() {
 	}
 
 	$.getJSON("./php/getFiles.php", function (data) {
-
-		$.each(data.regions, function (index, value) { 
-			var customLayer = L.geoJson(null, {
-				className: "region"
-			});
-			var runLayer = omnivore.kml("./data/Hueco/kml/regions/" + value, null, customLayer)
-			overlays.regions.addLayer(runLayer);
-		});
 
 		$.each(data.areas, function (index, value) { 
 			var customLayer = L.geoJson(null, {
@@ -60,17 +56,28 @@ $(document).ready(function() {
 		});
 
 		$.each(data.routes, function (index, value) { 
+
 			var customLayer = L.geoJson(null, {
-				className: "route"
+				pointToLayer: function (feature, latlng) {
+			        return L.circleMarker(latlng, {
+						className: "route",
+					    radius: 4,
+					    fillColor: "#ff7800",
+					    color: "#000",
+					    weight: 1,
+					    opacity: 1,
+					    fillOpacity: 0.8
+			        });
+			    }
 			});
 			var runLayer = omnivore.kml("./data/Hueco/kml/routes/" + value, null, customLayer)
 			overlays.routes.addLayer(runLayer);
 		});
 
 	}).done(function() {
-		overlays.regions.addTo(map);
-		overlays.areas.addTo(map);
-		overlays.trails.addTo(map);
+
+		//overlays.areas.addTo(map);
+		//overlays.trails.addTo(map);
 		//overlays.boulders.addTo(map);
 		//overlays.routes.addTo(map);
 	});
